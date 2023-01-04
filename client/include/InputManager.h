2d908d9bae6dcaf4e2facb6264bb6e66b2de18fb
Gal-Noy <galnoy1105@gmail.com>
2023-01-04 16:33:49 +0200

    implemented logout

--- client/include/InputManager.h
+++ client/include/InputManager.h
@@ -14,10 +14,10 @@ private:
 public:
     InputManager(ConnectionHandler*);
     void run();
-    void login(string &host_port, string &username, string &password);
-    void join(string &game_name);
-    void exit(string &game_name);
-    void report(string &file_path);
-    void summary(string &game_name, string &user, string &file_path);
+    void login(string &, string &, string &);
+    void join(string &);
+    void exit(string &);
+    void report(string &);
+    void summary(string &, string &, string &);
     void logout();
 };
--- client/src/InputManager.cpp
+++ client/src/InputManager.cpp
@@ -25,12 +25,11 @@ void InputManager::run(){
             exit(args[1]);
         else if (command == "report")
             report(args[1]);
-        // else if (command == "summary") 
-        //     summary(args[1], args[2], args[3]);
-        // else if (command == "logout")
-        //     logout();
+        else if (command == "summary") 
+            summary(args[1], args[2], args[3]);
+        else if (command == "logout")
+            logout();
     } 
-
 }
 
 void InputManager::login(string &host_port, string &username, string &password){
@@ -44,7 +43,7 @@ void InputManager::login(string &host_port, string &username, string &password){
 void InputManager::join(string &game_name){
     User &user = handler->getUser();
     string subscriptionId = std::to_string(user.getNextSID());
-    string receiptId = std::to_string(user.getNextRID());
+    string receiptId = "subscribe-" + std::to_string(user.getNextRID());
     SubscribeFrame frame(game_name, subscriptionId, receiptId);
     string line = frame.toString();
     handler->sendLine(line);
@@ -53,7 +52,7 @@ void InputManager::join(string &game_name){
 void InputManager::exit(string &game_name){
     User &user = handler->getUser();
     string subscriptionId = std::to_string(user.getSubscriptionId(game_name));
-    string receiptId = std::to_string(user.getNextRID());
+    string receiptId = "unsubscribe-" + std::to_string(user.getNextRID());
     UnsubscribeFrame frame(subscriptionId, receiptId);
     string line = frame.toString();
     handler->sendLine(line);
@@ -69,10 +68,23 @@ void InputManager::report(string &file_path){
 
     for (Event event : events){
         string body = "user: " + user.getUsername() + "\n" + event.toString();
-        string receiptId = std::to_string(user.getNextRID());
+        string receiptId = "report-" + std::to_string(user.getNextRID());
         SendFrame frame(destination, receiptId, body);
         string line = frame.toString();
         handler->sendLine(line);
     }
+}
+
+void InputManager::summary(string &game_name, string &user, string &file_path){
+    User &user = handler->getUser();
+}
+
+void InputManager::logout(){
+    User &user = handler->getUser();
+    string receiptId = "disconnect-" + std::to_string(user.getNextRID());
+    DisconnectFrame frame(receiptId);
+    string line = frame.toString();
+    handler->sendLine(line);
+    shouldTerminate = true;
 }
 
